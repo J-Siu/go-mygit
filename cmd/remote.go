43031f9d6b9bd39c03a48eb6cd324d346b7fb4f6
John, Sing Dao, Siu <john.sd.siu@gmail.com>
2022-05-09 03:05:00 -0400

    Cleanup comment

--- cmd/remote.go
+++ cmd/remote.go
@@ -35,14 +35,4 @@ var remoteCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(remoteCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// remoteCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// remoteCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
